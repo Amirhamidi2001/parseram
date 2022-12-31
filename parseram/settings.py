dc85d3cf0d3b37d99785cdf096cbdf870bee7296
Amirhamidi2001 <amirarsalanh2001@gmail.com>
2022-12-31 19:22:04 +0330

    Update project

--- parseram/settings.py
+++ parseram/settings.py
@@ -11,8 +11,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-import pymysql
-pymysql.install_as_MySQLdb()
+# import pymysql
+# pymysql.install_as_MySQLdb()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -81,24 +81,25 @@ WSGI_APPLICATION = "parseram.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'mydb',
-        'USER': 'root',
-        'PASSWORD': 'Arsalanh_2001',
-        'HOST':'localhost',
-        'PORT':'3306',
-    }
-}
-
-
 # DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": BASE_DIR / "db.sqlite3",
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'mydb',
+#         'USER': 'root',
+#         'PASSWORD': 'Arsalanh_2001',
+#         'HOST':'localhost',
+#         'PORT':'3306',
 #     }
 # }
+
+# https://www.djongomapper.com/using-django-with-mongodb-data-fields/
+
+DATABASES = {
+    "default": {
+        "ENGINE": "djongo",
+        "NAME": "mydb",
+    }
+}
 
 
 # Password validation
--- website/models.py
+++ website/models.py
@@ -8,7 +8,7 @@ class Category(models.Model):
     This class is for specifying product categories
     """
     title = models.CharField(max_length=255, null=True, blank=True)
-    parent = models.ForeignKey(
+    parent = models.EmbeddedField(
         'self',
         default=None,
         null=True,
@@ -28,7 +28,7 @@ class Product(models.Model):
     title = models.CharField(max_length=255)
     image = models.ImageField(null=True)
     content = models.CharField(max_length=255)
-    category = models.ForeignKey("Category", on_delete=models.CASCADE, null=True)
+    category = models.EmbeddedField("Category", on_delete=models.CASCADE, null=True)
     description = models.TextField(null=True)
 
     def __str__(self):
@@ -70,8 +70,8 @@ class OrderDetail(models.Model):
     """
     This class is for ordering
     """
-    o = models.ForeignKey("Order", on_delete=models.CASCADE, null=True)
-    product = models.ForeignKey("Product", on_delete=models.CASCADE)
+    o = models.EmbeddedField("Order", on_delete=models.CASCADE, null=True)
+    product = models.EmbeddedField("Product", on_delete=models.CASCADE)
     quantity = models.IntegerField(default=1)
     price = models.IntegerField(null=True)
 
@@ -83,7 +83,7 @@ class Order(models.Model):
     """
     This class is for finalizing the order
     """
-    user = models.ForeignKey("Customer", on_delete=models.CASCADE)
+    user = models.EmbeddedField("Customer", on_delete=models.CASCADE)
     date = models.DateField(null=True)
     totalprice = models.IntegerField(null=True)
 
