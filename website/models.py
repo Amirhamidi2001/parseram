dbe5c0bb9a755f4c6f46bf6bfa6601ed7ae3748c
parseram.user15 <parseram.user15@gmail.com>
2023-01-15 12:50:23 +0330

    Update models.py

--- website/models.py
+++ website/models.py
@@ -1,98 +1,4 @@
-# from django.db import models
-
-# from django.core.validators import MaxValueValidator, MinValueValidator
-
-
-# class Category(models.Model):
-#     """
-#     This class is for specifying product categories
-#     """
-#     title = models.CharField(max_length=255, null=True, blank=True)
-#     parent = models.ForeignKey(
-#         'self',
-#         default=None,
-#         null=True,
-#         blank=True,
-#         on_delete=models.CASCADE,
-#         related_name='child'
-#     )
-
-#     def __str__(self):
-#         return self.title
-
-
-# class Product(models.Model):
-#     """
-#     This class is for registering products
-#     """
-#     title = models.CharField(max_length=255)
-#     image = models.ImageField(null=True)
-#     content = models.CharField(max_length=255)
-#     category = models.ForeignKey("Category", on_delete=models.CASCADE, null=True)
-#     description = models.TextField(null=True)
-
-#     def __str__(self):
-#         return self.title
-
-
-# CITY_CHOICES = (
-#     ('Tehran', 'Tehran'),
-#     ('Shiraz', 'Shiraz'),
-#     ('Tabriz', 'Tabriz'),
-#     ('Mashhad', 'Mashhad'),
-#     ('Esfehan', 'Esfehan'),
-# )
-
-# GENDER_CHOICES = (
-#     ('Male', 'Male'),
-#     ('Female', 'Female'),
-# )
-
-
-# class Customer(models.Model):
-#     """
-#     This class is for storing customer information
-#     """
-#     name = models.CharField(max_length=255)
-#     family = models.CharField(max_length=255)
-#     age = models.IntegerField(
-#         validators=[MinValueValidator(14), MaxValueValidator(100)]
-#     )
-#     city = models.CharField(choices=CITY_CHOICES, max_length=10)
-#     address = models.TextField(null=True)
-#     gender = models.CharField(choices=GENDER_CHOICES, max_length=10)
-
-#     def __str__(self):
-#         return self.name
-
-
-# class OrderDetail(models.Model):
-#     """
-#     This class is for ordering
-#     """
-#     o = models.ForeignKey("Order", on_delete=models.CASCADE, null=True)
-#     product = models.ForeignKey("Product", on_delete=models.CASCADE)
-#     quantity = models.IntegerField(default=1)
-#     price = models.IntegerField(null=True)
-
-#     def __str__(self):
-#         return f"{self.quantity} of {self.product.title}"
-
-
-# class Order(models.Model):
-#     """
-#     This class is for finalizing the order
-#     """
-#     user = models.ForeignKey("Customer", on_delete=models.CASCADE)
-#     date = models.DateField(null=True)
-#     totalprice = models.IntegerField(null=True)
-
-#     def __str__(self):
-#         return self.user.name
-
 from django.db import models
-
-# Create your models here.
 
 
 class Category(models.Model):
@@ -105,7 +11,6 @@ class Category(models.Model):
         on_delete=models.CASCADE,
         related_name='child'
     )
-
 
     def __str__(self):
         return self.title
