7b84cf45414b965fc3ff071ca2f97a48bd82105c
parseram.user15 <parseram.user15@gmail.com>
2023-01-18 15:30:54 +0330

    Update settings.py
    Update database settings

--- parseram/settings.py
+++ parseram/settings.py
@@ -97,17 +97,9 @@ WSGI_APPLICATION = "parseram.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "djongo",
-        "NAME": "mydb",
+        "NAME": "parseram",
     }
 }
-
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": BASE_DIR / "db.sqlite3",
-#     }
-# }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
